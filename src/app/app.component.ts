5ade0618d287dba3e54eaae0ec123c662471fcb2
Viswanath Mucheli Chenchu <mcviswanathmca@gmail.com>
2021-04-20 12:56:24 -0400

    final changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'angular-forms';
+export class AppComponent implements OnInit {
+  title!: string;
+
+  myForm = new FormGroup({
+    firstName: new FormControl({
+      value: '',
+      disabled: false,
+    }),
+
+    lastName: new FormControl({
+      value: '',
+      disabled: true,
+    }),
+  });
+
+  mySubmitFunction(event: any): void {
+    event.preventDefault();
+
+    console.log(JSON.stringify(this.myForm.value));
+  }
+
+  ngOnInit(): void {
+    const firstNameControl = this.myForm.get('firstName') as FormControl;
+    const lastNameControl = this.myForm.get('lastName') as FormControl;
+
+    this.title = 'Viswa is Awesome'; // it's true
+
+    firstNameControl.valueChanges.subscribe((fn) => {
+      if (fn.length > 0) {
+        lastNameControl.enable();
+      } else {
+        lastNameControl.disable();
+      }
+    });
+  }
 }
